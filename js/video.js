2c036c5d95bd99b8a5785567413638d62c65ed66
Zachary Taylor <zacharytaylor@tutamail.com>
2022-12-04 11:11:50 -0600

    Adding considerations for html 5 and motion

--- js/video.js
+++ js/video.js
@@ -1,2 +1,11 @@
-const video = document.querySelector('video')
-video.setAttribute('autoplay', '')
+const video = document.querySelector('video');
+const motionQuery = matchMedia('(prefers-reduced-motion)');
+function handleReduceMotionChanged() {
+  if (motionQuery.matches) {
+    video.setAttribute('controls', '');
+  } else {
+    video.setAttribute('autoplay', '');
+  }
+}
+motionQuery.addEventListener('change', handleReduceMotionChanged);
+handleReduceMotionChanged();
